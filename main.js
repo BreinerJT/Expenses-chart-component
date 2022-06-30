2f986a32121533f9571a724b06be530dea83785d
BreinerJT <breinerjtorres@gmail.com>
2022-06-30 18:32:05 -0500

    Final

--- main.js
+++ main.js
@@ -3,24 +3,29 @@ const day = new Date().getDay() - 1
 async function getData(url){
     let fetchData = fetch(url)
     let response = await fetchData
+    if(!response.ok) return
     let data = await response.json()
     return data;
 }
 
 async function getCharts(){
-    const charts = document.querySelectorAll(".js-chart")
-    const labels = document.querySelectorAll(".js-label")
-    const data = await getData('data.json')
+    const charts = document.querySelector(".charts");
+    const data = await getData('data.json');
+
     const amounts = data.map(e => e.amount)
     let maxValue = Math.max(...amounts)
-    let heights = amounts.map(e => e / maxValue * 100)
-    charts.forEach((value, index) => {
-        value.style.height = `${heights[index]}%`
-    })
-    charts[day].classList.add("current-day")
-    labels.forEach((value, index) => {
-        value.textContent = `$${amounts[index]}`
-    })
+    let html = null;
+    data.forEach(e => {
+        html =  `
+            <div class="chart-container">
+                <p class="day" data-id="${e.day}">${e.day}</p>
+                <div class="js-chart chart" id="${e.day}" style="height: ${(e.amount) / maxValue * 100}%"></div>
+                <label class="js-label" for="${e.day}">${e.amount}</label>
+            </div>
+            `
+    charts.insertAdjacentHTML("afterbegin", html);
+});
+    const chart = document.querySelectorAll(".js-chart")
+    chart[day].classList.add("current-day")
 }
-
 getCharts()
